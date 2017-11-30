d3edc334b07c2416042bd4410d2b07ed2da5ded6
rendj <ren1101831987@sina.com>
2017-11-30 22:13:04 +0800

    222

--- src/main/java/com/ren/SpringbootDemo4Application.java
+++ src/main/java/com/ren/SpringbootDemo4Application.java
@@ -3,6 +3,9 @@ package com.ren;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class SpringbootDemo4Application {
 
